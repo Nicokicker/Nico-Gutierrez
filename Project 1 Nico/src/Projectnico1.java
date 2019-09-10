72c1223836d062c773fc296635aeceb1ff5da80a
nicolasgtz <nicolasgtz@10.242.0.73>
2019-09-09 23:09:09 -0400

    Ready for Review.

---
+++ Project 1 Nico/src/Projectnico1.java
@@ -0,0 +1,23 @@
+
+public class Projectnico1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Project Name= Project Nico 1
+		// Name= Nicolas Gutierrez
+		
+		System.out.println("I'm super ready for September 14 Gameday."); 
+		System.out.println("We are gonna win");
+		System.out.println("My family is proud of me");
+		System.out.println("My number is 97");
+		System.out.println("I am a kicker");
+		System.out.println("I am the only International student");
+		System.out.println("I'm from Mexico");
+		System.out.println("I've been playing football since 3 years ago");
+		System.out.println("It is gonna be my first game");
+		System.out.println("I'm really happy");
+
+	}
+
+}
