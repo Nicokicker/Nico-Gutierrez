b3f98d19ca8b577b00968a9d2a36704da682f8ce
nicolasgtz <nicolasgtz@10.242.0.243>
2019-10-23 10:24:19 -0400

    Ready to check, final version!!

--- Program 2/src/Program2.java
+++ Program 2/src/Program2.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Program2 {
 
 
-	private static Scanner in;
+	public static Scanner in;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
